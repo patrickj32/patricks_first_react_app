f3a6eab5b568a20e1954cec8c554ed4ad3cca8cf
Patrick Justice <patrickjustice@Patricks-MacBook-Pro-2.local>
2022-03-30 19:30:49 -0400

    created starter componets

---
+++ src/pj_components/CustomGreeting.js
@@ -0,0 +1,10 @@
+function CustomGreeting (props) {
+
+    return (
+        <div className="CustomGreeting">
+            <h1>Greetings, {props.userName}</h1>
+        </div>
+    );
+}
+
+export default CustomGreeting ;
---
+++ src/pj_components/Welcome.js
@@ -0,0 +1,10 @@
+function Welcome(){
+
+    return (
+        <div className="Welcome">
+            <h1>Welcome, Patrick</h1>
+        </div>
+    );
+}
+
+export default Welcome;
